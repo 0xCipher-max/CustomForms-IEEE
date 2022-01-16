1af33d2ba241a0b0ef39ed0975eb97ad6e0176db
0xCipher-max <hr607656@gmail.com>
2022-01-16 18:22:56 +0530

    Added static

--- form/urls.py
+++ form/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static 
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
     path('favicon.ico', RedirectView.as_view(url = staticfiles_storage.url('/Icon/logo.png')))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from index.models import *;
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Choices)
+admin.site.register(Questions)
+admin.site.register(Answer)
+admin.site.register(Form)
+admin.site.register(Responses)
