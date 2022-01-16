0c976566cb5eda659916b085495b4bd4d9eb1d5b
0xCipher-max <hr607656@gmail.com>
2022-01-16 18:26:01 +0530

    Added static new

--- form/settings.py
+++ form/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.join(BASE_DIR, 'static');
+STATIC_ROOT = os.join(BASE_DIR, 'staticfiles');
 
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
